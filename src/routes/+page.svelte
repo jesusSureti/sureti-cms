b304ece314dfdc826e9b9aacc86d1a407d54a65f
jesusSureti <141356514+jesusSureti@users.noreply.github.com>
2024-06-04 11:40:03 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<h1>Welcome to The admin</h1>
+<p>Introduce your user information</p>
+
+
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+
+    let addingNewFaq: boolean = false;
+</script>
+
+<h1>Dashboard</h1>
+
+<button on:click={() => (addingNewFaq = true)}>Add a new Faq</button>
+
+{#if addingNewFaq}
+    <div>
+        <label for="">Titulo</label>
+        <input type="text" placeholder="título del Faq">
+
+        <label for="">Cuerpo</label>
+        <input type="text" placeholder="Resp/Descrip">
+
+        <label for="">Tags</label>
+        <input type="text" placeholder="tag">
+
+        <!-- Debe ser un select con options -->
+        <label for="">Categoria</label>
+        <input type="text" placeholder="Inve/Prest/"> 
+
+        <!-- Debe ser un select con options -->
+        <label for="">Plataforma</label> 
+        <input type="text" placeholder="título del Faq">
+
+        <label for="">Author</label>
+        <input type="text" placeholder="título del Faq">
+
+    </div>
+    <!-- <Modal
+        {showModal}
+        on:close={(e) => {
+            showModal = false;
+        }}
+    >
+        <div slot="header">
+            <div class="modal-header">
+                <h4>Agrega el nuevo Faq</h4>
+            </div>
+        </div>
+        <div slot="body">
+            <div class="modal-body">
+                
+                
+            </div>
+        </div>
+    </Modal> -->
+{/if}
